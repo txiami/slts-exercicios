74e4afb85cde8a5bcd341c5655029d3dcf46f39e
Hugo Almeida <hugomurilo.almeida@gmail.com>
2023-08-07 19:13:32 -0300

    updtade Hugo

---
+++ Lista03/_16_Lista03/src/app/Program.java
@@ -0,0 +1,93 @@
+package app;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o número do dia e do mês do seu aniverário:");
+		int dia = sc.nextInt();
+		int mes = sc.nextInt();
+		String nomeMes = "";
+		sc.close();
+
+		switch (mes) {
+		case 1:
+			nomeMes = "janeiro";
+			break;
+		case 2:
+			nomeMes = "fevereiro";
+			break;
+		case 3:
+			nomeMes = "março";
+			break;
+		case 4:
+			nomeMes = "abril";
+			break;
+		case 5:
+			nomeMes = "maio";
+			break;
+		case 6:
+			nomeMes = "junho";
+			break;
+		case 7:
+			nomeMes = "julho";
+			break;
+		case 8:
+			nomeMes = "agosto";
+			break;
+		case 9:
+			nomeMes = "setembro";
+			break;
+		case 10:
+			nomeMes = "outubro";
+			break;
+		case 11:
+			nomeMes = "novembro";
+			break;
+		case 12:
+			nomeMes = "dezembro";
+			break;
+		default:
+			System.out.println("valor inválido");
+		}
+
+		boolean dataValida = validarData(dia, mes);
+		String trimestre = retornaTrimestre(mes);
+		
+		if (dataValida) {
+			System.out.println("A data é válida. O seu aniversário é em " + nomeMes + ", dia " + dia);
+			System.out.println(trimestre);
+		} else {
+			System.out.println("A data é inválida.");
+		}
+	}
+
+	public static boolean validarData(int dia, int mes) {
+		if (mes < 1 || mes > 12) {
+			return false;
+		} else if (dia < 1 || dia > 31) {
+			return false;
+		} else if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia > 30) {
+			return false;
+		}else if (mes == 2) {
+			if (dia > 28) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static String retornaTrimestre(int mes) {
+        if (mes >= 1 && mes <= 3) {
+            return "Nasceu no 1º trimestre";
+        } else if (mes >= 4 && mes <= 6) {
+            return "Nasceu no 2º trimestre";
+        } else if (mes >= 7 && mes <= 9) {
+            return "Nasceu no 3º trimestre";
+        } else {
+            return "Nasceu no 4º trimestre";
+        }
+    }
+}
---
+++ Lista03/_57_List03/src/app/Program.java
@@ -0,0 +1,33 @@
+package app;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Informe a primeira data no formato (dd/mm/aaaa): ");
+		String data1 = sc.nextLine();
+		System.out.print("Informe a segunda data no formato (dd/mm/aaaa): ");
+		String data2 = sc.nextLine();
+		
+		DateTimeFormatter formatar = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate data1Formatada = LocalDate.parse(data1, formatar);
+		LocalDate data2Formatada = LocalDate.parse(data2, formatar);
+		
+		procurarDatasEntre(data1Formatada, data2Formatada);
+		
+		sc.close();
+	}
+
+	public static void procurarDatasEntre(LocalDate data1, LocalDate data2) {
+		LocalDate proximaData = data1.plusDays(1);
+
+        while (proximaData.isBefore(data2)) {
+            System.out.println(proximaData.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+            proximaData = proximaData.plusDays(1);
+        }
+	}
+}
