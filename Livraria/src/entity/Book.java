fc5572287103260369ee4a3a5bb1463557e1aeda
francivaldolima <francivaldolimadesousa@gmail.com>
2023-08-11 23:59:14 -0300

    Adicionadas as classes Livro e Venda

---
+++ Livraria/src/entity/Book.java
@@ -0,0 +1,10 @@
+package entity;
+
+public class Book extends Livro{
+
+    public Book(String titulo, String autores, String editora, float preco) {
+        super(titulo, autores, editora, preco);
+    }
+
+
+}
